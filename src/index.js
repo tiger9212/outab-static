505f4600ffd0cd9fcd2b271036c3d4c3d6284f29
zhouyi <magic_023@163.com>
2019-01-25 14:10:19 +0800

    删除多余数据

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Login from "./view/login/index";
-import Common from "./layout/Common";
 import {Route} from 'react-router';
 import {HashRouter} from 'react-router-dom';
 import './index.css';
@@ -12,7 +11,6 @@ class Main extends React.Component{
         return(<HashRouter>
             <div>
                 <Route path="/login" component={Login} />
-                <Route path="/blog" component={Common}/>
             </div>
         </HashRouter>)
     }
